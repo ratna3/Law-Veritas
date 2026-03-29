62ef38d7cf1c8326bf0f29d49a7ae0ee7949f1f0
Ratna Kirti <ratnakirtiscr@gmail.com>
2026-03-30 03:19:41 +0530

    Move breaking news ticker below navbar header, fix animation to start from extreme right

--- my-right-window/src/pages/Home.jsx
+++ my-right-window/src/pages/Home.jsx
@@ -4,7 +4,6 @@ import BlogCard from '../components/blog/BlogCard';
 import { ScaleIcon, GavelIcon, FileTextIcon, UsersIcon } from '../components/Icons';
 import { useBlogStore } from '../store';
 import useIntersectionObserver from '../hooks/useIntersectionObserver';
-import NewsTicker from '../components/common/NewsTicker';
 import { FaBalanceScale, FaGavel, FaBookOpen, FaShieldAlt, FaHandshake, FaLandmark, FaArrowRight, FaLinkedinIn, FaInstagram } from 'react-icons/fa';
 
 const services = [
@@ -137,11 +136,6 @@ const Home = () => {
           </div>
         </div>
       </section>
-
-      {/* ═══════════════════════════════════════════════════════
-          BREAKING NEWS TICKER
-          ═══════════════════════════════════════════════════════ */}
-      <NewsTicker />
 
       {/* ═══════════════════════════════════════════
           PRACTICE AREAS — 4-col with SVG Icons
